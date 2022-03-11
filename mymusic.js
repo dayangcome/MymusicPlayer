05521289b417722f93a444b832348b8a9ba79723
dayangcome <87306181+dayangcome@users.noreply.github.com>
2022-03-11 21:03:42 +0800

    first add files
    first add files

---
+++ mymusic.js
@@ -0,0 +1,52 @@
+var app=new Vue({
+	el:"#app",
+	data:{
+		query:"",
+		musiclist:[],
+		musicurl:"",
+		cover:"",
+		hotcom:[],
+		show:false,
+		mvurl:"",
+		isshow:false
+	},
+	methods:{
+		searchmusic:function(){
+			var that=this
+			axios.get("https://autumnfish.cn/search?keywords="+this.query)
+			.then(function(response){
+				that.musiclist=response.data.result.songs;
+			},function(err){})
+		},
+		play:function(musicid){
+			var that = this
+			this.show=true
+			axios.get("https://autumnfish.cn/song/url?id="+musicid)
+			.then(function(response){
+				that.musicurl=response.data.data[0].url
+			},function(err){
+				
+			})
+			axios.get("https://autumnfish.cn/song/detail?ids="+musicid)
+			.then(function(response){
+				that.cover=response.data.songs[0].al.picUrl
+			},function(err){
+			})
+			axios.get("https://autumnfish.cn/comment/hot?type=0&id="+musicid)
+			.then(function(response){
+				that.hotcom=response.data.hotComments
+			},function(err){})
+		},
+		playmv:function(mvid){
+			var that=this
+			axios.get("https://autumnfish.cn/mv/url?id="+mvid)
+			.then(function(response){
+				that.isshow=true
+				that.mvurl=response.data.data.url
+			},function(err){})
+		},
+		pauseshow:function(){
+			this.isshow=false
+		}
+	}
+})
